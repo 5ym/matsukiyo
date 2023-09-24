2428114a706474f719f9274b828ab56c02748a0b
Ruk Doe <info@doany.io>
2023-09-24 23:13:14 +0900

    fix

---
+++ cu.ts
@@ -0,0 +1,26 @@
+import * as fs from 'fs'
+const url = 'https://www.matsukiyo.co.jp/map/s3/json/'
+let list: any
+let attr: any
+
+Promise.all([
+    fetch(url+'stores.json').then((data) => data.json()).then((json) => {
+        list = json.filter(function(item){
+            if (/\d{8}0\d{2}/.test(item.services)) return true
+        });
+    }),
+    fetch(url+'storeAttributes.json').then((data) => data.json()).then((json) => {
+        attr = json
+    })
+]).finally(() => {
+    let body = ""
+    list.forEach((item: any) => {
+        let services = ""
+        item.services.split('').forEach((i, k) => {
+            if(i == 1 && attr.services[k][2]) services += "<img src='https://www.matsukiyo.co.jp/map/s3/icon/"+attr.services[k][2]+"'>"
+        })
+        body += "<tr><td>"+item.name+"</td><td>"+item.address+"</td><td>"+item.closed_day+"</td><td>"+services+"</td><td><a target='_blank' href='https://www.matsukiyo.co.jp/map?kid="+item.id+"'>https://www.matsukiyo.co.jp/map?kid="+item.id+"</a></td></tr>\n"
+    })
+    console.log(body)
+    fs.writeFileSync('./list.html', body)
+})
